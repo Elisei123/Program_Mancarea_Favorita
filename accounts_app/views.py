49e032e937817feafba1dd0e73d1c672dbdfd83f
elisei <nicolae.elisei1999@gmail.com>
2020-01-04 21:31:40 +0200

    Update errors show and verify username and email valid

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -8,32 +8,63 @@ from django.contrib.auth import authenticate
 from django.db.models.functions import Lower
 # Create your views here.
 
+def is_email(string):
+    from django.core.exceptions import ValidationError
+    from django.core.validators import EmailValidator
+
+    validator = EmailValidator()
+    try:
+        validator(string)
+    except ValidationError:
+        return False
+
+    return True
+
+def UsernameValidator(string):
+    from django.contrib.auth.validators import UnicodeUsernameValidator
+    validator = UnicodeUsernameValidator()
+    try:
+        validator(string)
+    except ValidationError:
+        return False
+
+    return True
+
+
+
 def register(request):
     if request.user.is_authenticated:
         return redirect('home')
     else:
         if request.method == "POST":
             username = request.POST['username'].lower()
-            email = request.POST['email'].lower()
-            v = validate_email(email)
-            print(v)
-            password1 = request.POST['password1']
-            password2 = request.POST['password2']
-            if password1 == password2:
-                if User.objects.filter(username=username).exists():
-                    messages.info(request, 'Username-ul este folosit.')
-                    return redirect('register')
-                elif User.objects.filter(email=email).exists():
-                    messages.info(request, 'Email-ul este folosit.')
-                    return redirect('register')
+            if UsernameValidator(username):
+                email = request.POST['email'].lower()
+
+                if is_email(email):
+                    password1 = request.POST['password1']
+                    password2 = request.POST['password2']
+                    if password1 == password2:
+                        if User.objects.filter(username=username).exists():
+                            messages.info(request, 'Username-ul este folosit.')
+                            return redirect('register')
+                        elif User.objects.filter(email=email).exists():
+                            messages.info(request, 'Email-ul este folosit.')
+                            return redirect('register')
+                        else:
+                            user = User.objects.create_user(username=username, password=password1, email=email)
+                            user.save()
+                            print("User created")
+                            messages.success(request, 'Contul a fost creat cu succes!')
+                            return redirect('login')
+                    else:
+                        messages.info(request, 'Parola de verificare nu este la fel.')
+                        return redirect('register')
                 else:
-                    user = User.objects.create_user(username=username, password=password1, email=email)
-                    user.save()
-                    print("User created")
-                    messages.info(request, 'Contul a fost creat cu succes!')
-                    return redirect('login')
+                    messages.info(request, 'Email-ul nu este valid ("exemplu@gmail.com")!')
+                    return redirect('register')
             else:
-                messages.info(request, 'Parola de verificare nu este la fel.')
+                messages.info(request, 'Username-ul contine spatiu sau caractere interzise!')
                 return redirect('register')
         else:
             return render(request, "register.html")
@@ -53,7 +84,7 @@ def login(request):
                 auth.login(request, user)
                 return redirect("/")
             else:
-                messages.info(request, "Invalid username or password")
+                messages.error(request, "Username-ul sau parola nu sunt valide!")
                 return redirect('login')
 
         else:
